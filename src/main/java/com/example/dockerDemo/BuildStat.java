bb1e1f5ee9b035b4b9a6b9658305a9513b073604
suwnam <sw.nam@synetics.kr>
2025-07-28 09:18:14 +0900

    build: add libraries

--- src/main/java/com/example/dockerDemo/BuildStat.java
+++ src/main/java/com/example/dockerDemo/BuildStat.java
@@ -1,3 +1,9 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
 @Entity
 public class BuildStat {
     @Id @GeneratedValue
--- src/main/java/com/example/dockerDemo/BuildStatController.java
+++ src/main/java/com/example/dockerDemo/BuildStatController.java
@@ -1,3 +1,8 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 @RestController
 @RequestMapping("/stats")
 public class BuildStatController {
