681f3b4d552f4920536f6892f33c09101f896478
suwnam <sw.nam@synetics.kr>
2025-07-28 14:29:02 +0900

    fix: add table annotaion

--- src/main/java/com/example/dockerDemo/entity/BuildStat.java
+++ src/main/java/com/example/dockerDemo/entity/BuildStat.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "build_stat")
 public class BuildStat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,6 +13,8 @@ public class BuildStat {
     private String jobName;
     private String status; // SUCCESS, FAILURE 등
     private long duration; // milliseconds
+
+    @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime timestamp;
 
     // Getters/Setters 생략 가능 (Lombok 사용 가능)
